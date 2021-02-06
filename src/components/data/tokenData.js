1641ad7a562e257133af7379e9bfc1a5d2cffcc7
James Mitchell <iamjmitch1990@gmail.com>
2021-02-07 00:23:05 +1000

    path update

--- src/components/data/tokenData.js
+++ src/components/data/tokenData.js
@@ -5,19 +5,19 @@ const shadowBoxSettings = "0px 10px 0px 0px "
 export const tokenData = [
   {
     name: "paper",
-    iconURL: "./icon-paper.svg",
+    iconURL: "./static/icon-paper.svg",
     gradient: colors.paper,
     shadow: shadowBoxSettings + "#2945c3",
   },
   {
     name: "scissors",
-    iconURL: "./icon-scissors.svg",
+    iconURL: "./static/icon-scissors.svg",
     gradient: colors.scissors,
     shadow: shadowBoxSettings + "#c76c1b",
   },
   {
     name: "rock",
-    iconURL: "./icon-rock.svg",
+    iconURL: "./static/icon-rock.svg",
     gradient: colors.rock,
     shadow: shadowBoxSettings + "#9d1634",
   },
